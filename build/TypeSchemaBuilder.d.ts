94241cfce8aeeee4ed75c3e1c8e9cfad5801965e
nfour <novus.nfour@gmail.com>
2017-09-28 10:44:16 +1000

    refs

---
+++ build/TypeSchemaBuilder.d.ts
@@ -0,0 +1,31 @@
+import { ISaveSchemasConfig, ITjsSchema, ITypesToSchemasConfig } from './typesToSchemas';
+export interface IBuilderSchemaConfig {
+    save?: Partial<ISaveSchemasConfig>;
+    compile?: Partial<ITypesToSchemasConfig>;
+}
+export declare class TypeSchemaBuilder {
+    compiled: Array<{
+        schemas: ITjsSchema[];
+        config: IBuilderSchemaConfig;
+        errors?: Error[];
+    }>;
+    private saveConfig?;
+    private compileConfig?;
+    private builderConfigs;
+    constructor({save, compile}: {
+        save?: TypeSchemaBuilder['saveConfig'];
+        compile?: TypeSchemaBuilder['compileConfig'];
+    });
+    add(schemaConfig: IBuilderSchemaConfig): this;
+    compile(): Promise<{
+        schemas: ITjsSchema[];
+        config: IBuilderSchemaConfig;
+        errors?: Error[] | undefined;
+    }[]>;
+    save(compiled?: {
+        schemas: ITjsSchema[];
+        config: IBuilderSchemaConfig;
+        errors?: Error[] | undefined;
+    }[]): Promise<void>;
+    compileAndSave(): Promise<this>;
+}
--- build/typesToSchemas.d.ts
+++ build/typesToSchemas.d.ts
@@ -1,34 +1,34 @@
 import * as TJS from 'typescript-json-schema';
 import { IGetImportPath } from './lib/schemaRenderers';
 export interface ITjsSchema {
-    key: string;
+    name: string;
     type: string;
     schema: any;
 }
 export declare const defaultOptions: Partial<TJS.Args>;
+export interface ITypesToSchemasConfig {
+    /** The TS files to fetch types from */
+    fromFiles: string[];
+    /** A hash of { [exportName]: typeName } */
+    types: Array<{
+        name: string;
+        type: string;
+        /** Optionally, set a default schema id */
+        id?: string;
+    }>;
+    /** TJS options to override */
+    options?: Partial<TJS.Args>;
+    dereference?: boolean;
+}
 /**
  * Reads TypeScript files using `typescript-json-schema` and returns both
  * errors and the resulting schemas.
  */
-export declare function typesToSchemas({fromFiles, types, id, options}: {
-    /** The TS files to fetch types from */
-    fromFiles: string[];
-    /** A hash of { [exportName]: typeName } */
-    types: {
-        [key: string]: string;
-    };
-    /** Optionally, set a default schema id */
-    id?: string;
-    /** TJS options to override */
-    options?: Partial<TJS.Args>;
-}): Promise<{
+export declare function typesToSchemas({fromFiles, types, options, dereference: doDereference}: ITypesToSchemasConfig): Promise<{
     errors?: Error[];
     schemas: ITjsSchema[];
 }>;
-/**
- * Saves schemas as multiple format, to a `.ts` or `.json` file, based on provided options.
- */
-export declare function saveSchemas({schemas, directory, name, format, asDefaultExport}: {
+export interface ISaveSchemasConfig {
     /** The file format to save as  */
     format: 'ts' | 'json';
     /**
@@ -42,7 +42,11 @@ export declare function saveSchemas({schemas, directory, name, format, asDefault
     /** The absolute path to the save directory */
     directory: string;
     schemas: ITjsSchema[];
-}): Promise<void>;
+}
+/**
+ * Saves schemas as multiple format, to a `.ts` or `.json` file, based on provided options.
+ */
+export declare function saveSchemas({schemas, directory, name, format, asDefaultExport}: ISaveSchemasConfig): Promise<void>;
 /**
  * Creates an index file wiring up imports and exports
  */
